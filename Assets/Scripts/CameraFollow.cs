70618bb96cf314547e3204dd29e9f33aaa1bf5d4
Oscar Dominguez Alonso <iamoscaar@gmail.com>
2026-04-30 13:28:07 +0200

    Añadir mecánicas de colisión, prefabs y seguimiento de cámara

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
