eac16e3a62a25339437943e3f137082162ce3ee1
Oscar Dominguez Alonso <iamoscaar@gmail.com>
2026-05-05 14:07:12 +0200

    Restaurar Player.cs a versión de main

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,11 @@
 using System;
+using System.Numerics;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
@@ -16,6 +22,9 @@ public class Player : MonoBehaviour
     public LayerMask groundLayer;
 
     private Animator animator;
+
+    private int coins;
+    public TMP_Text textCoins;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -41,11 +50,46 @@ public class Player : MonoBehaviour
         }
         
         animator.SetFloat("Speed", Mathf.Abs(move));
-        
+        animator.SetFloat("VerticalVelocity",rb2D.linearVelocity.y);
+        animator.SetBool("isGrounded", isGrounded);
     }
 
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+    }
+
+    // Trigger for colisions / chocar con monedas spikes barriles etc.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+            coins++;
+            textCoins.text = coins.ToString();
+        }
+
+        if (collision.transform.CompareTag("Spikes"))
+        {
+            // Esto reinicia el juego, a la escena primera.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+        if (collision.transform.CompareTag("Barrel"))
+        {
+            Vector2 knockbackDir = (rb2D.position - (Vector2)collision.transform.position).normalized;
+            rb2D.linearVelocity = Vector2.zero;
+            rb2D.AddForce(knockbackDir*3,ForceMode2D.Impulse);
+
+            BoxCollider2D[] colliders = collision.gameObject.GetComponents<BoxCollider2D>();
+
+            foreach (BoxCollider2D col in colliders)
+            {
+                col.enabled = false;
+            }
+
+            collision.GetComponent<Animator>().enabled = true;
+            Destroy(collision.gameObject, 0.5f);
+        }
     }
 }
