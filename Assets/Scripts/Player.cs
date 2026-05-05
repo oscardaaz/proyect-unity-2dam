8a8021146ad058d29d3d232c22fbfc76c1f64d75
Oscar Dominguez Alonso <iamoscaar@gmail.com>
2026-05-05 14:37:08 +0200

    Actualizar Player.cs refactorizado, escena y paquetes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,95 +1,71 @@
-using System;
-using System.Numerics;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using Vector2 = UnityEngine.Vector2;
-using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
-
     public float speed = 5;
-    private Rigidbody2D rb2D;
-    
-    private float move;
-
     public float jumpForce = 4;
-    private bool isGrounded;
     public Transform groundCheck;
     public float groundRadius = 0.1f;
     public LayerMask groundLayer;
 
+    private Rigidbody2D rb2D;
     private Animator animator;
+    private PlayerHealth health;
+    private float move;
+    private bool isGrounded;
 
-    private int coins;
-    public TMP_Text textCoins;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        health = GetComponent<PlayerHealth>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         move = Input.GetAxisRaw("Horizontal");
-        rb2D.linearVelocity = new Vector2(move*speed, rb2D.linearVelocity.y);
+        rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
 
         if (move != 0)
-        {
-            transform.localScale = new Vector3(Mathf.Sign(move),1, 1);
-        }
+            transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
-        {
             rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
-        }
-        
+
         animator.SetFloat("Speed", Mathf.Abs(move));
-        animator.SetFloat("VerticalVelocity",rb2D.linearVelocity.y);
+        animator.SetFloat("VerticalVelocity", rb2D.linearVelocity.y);
         animator.SetBool("isGrounded", isGrounded);
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 
-    // Trigger for colisions / chocar con monedas spikes barriles etc.
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.transform.CompareTag("Coin"))
+        if (collision.CompareTag("Coin"))
         {
             Destroy(collision.gameObject);
-            coins++;
-            textCoins.text = coins.ToString();
+            GameManager.Instance.AddCoin();
         }
 
-        if (collision.transform.CompareTag("Spikes"))
-        {
-            // Esto reinicia el juego, a la escena primera.
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        
-        if (collision.transform.CompareTag("Barrel"))
+        if (collision.CompareTag("Spikes"))
+            health.TakeDamage();
+
+        if (collision.CompareTag("Barrel"))
         {
             Vector2 knockbackDir = (rb2D.position - (Vector2)collision.transform.position).normalized;
             rb2D.linearVelocity = Vector2.zero;
-            rb2D.AddForce(knockbackDir*3,ForceMode2D.Impulse);
+            rb2D.AddForce(knockbackDir * 3, ForceMode2D.Impulse);
 
-            BoxCollider2D[] colliders = collision.gameObject.GetComponents<BoxCollider2D>();
-
-            foreach (BoxCollider2D col in colliders)
-            {
+            foreach (BoxCollider2D col in collision.gameObject.GetComponents<BoxCollider2D>())
                 col.enabled = false;
-            }
 
             collision.GetComponent<Animator>().enabled = true;
             Destroy(collision.gameObject, 0.5f);
+
+            health.TakeDamage();
         }
     }
 }
